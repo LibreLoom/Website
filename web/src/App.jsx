47380d8d95833ae119cc44553e9d613be38a112b
Xu Ning <thuningxu@github>
2025-11-10 20:50:45 +0000

    fixed import

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import useState from 'react'
+import { useState } from 'react'
 import Navigation from './components/Navigation'
 import Home from './components/Home'
 import Projects from './components/Projects'
